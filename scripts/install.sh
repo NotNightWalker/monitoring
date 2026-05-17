6d1bacf216c1e4193f406b42a3650589fd7deff4
NotNightWalker <timofei.beznaev@yandex.ru>
2026-05-17 16:09:28 +0300

    Добавлен скрипт установки и настройка systemd

---
+++ scripts/install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+# Проверка прав
+if [ "$EUID" != 0 ]; then
+    echo "Недостаточно прав."
+    exit 1
+fi
+
+# Создаем пользователя и папку
+id my-monitoring &>/dev/null || useradd my-monitoring -r -s /usr/sbin/nologin
+mkdir -p /opt/monitoring/
+chown -R my-monitoring:my-monitoring /opt/monitoring/
+chmod 750 /opt/monitoring/
+
+# Копируем папки и requirements
+cp -r ../src /opt/monitoring/
+cp -r ../config /opt/monitoring/
+cp -r ../requirements.txt /opt/monitoring/
+cp ../systemd/monitoring.service /etc/systemd/system/
+cp -r ../scripts /opt/monitoring/
+cd /opt/monitoring/ 
+
+# Устанавливаем зависимости
+python3 -m venv venv # Выполняется от root, так как скрипт запускает root
+chown -R my-monitoring:my-monitoring venv # Меняем владельца для папки venv
+source venv/bin/activate
+pip install -r requirements.txt
+deactivate
+
+# Настройка systemd
+sudo systemctl daemon-reload
+sudo systemctl enable monitoring
+sudo systemctl start monitoring
